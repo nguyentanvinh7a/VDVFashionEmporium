e9f6a8cf06a5945c58a4b0ad08675e38a117ac86
nguyentanvinh7a <43849741+nguyentanvinh7a@users.noreply.github.com>
2020-11-15 16:42:17 +0700

    Đổ dữ liệu vào chi tiết SP, bổ sung vài liên kết

--- models/productModel.js
+++ models/productModel.js
@@ -3,52 +3,62 @@ exports.list = () => {
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         },
         {
             nameProduct: 'SET ĐỒ LƯỚI ÔM',
             price: '210.000',
-            productDescription: 'No description for this product.'
+            productDescription: 'No description for this product.',
+            img: 'http://product.hstatic.net/1000290753/product/untitled_session2751_df891e66ca9442daa47100bce2e68478_master.jpg'
         }
     ]
 }
