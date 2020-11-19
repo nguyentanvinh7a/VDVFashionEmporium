01d4293457847cb441d32996618bf9c65a1c2324
nguyentanvinh7a <43849741+nguyentanvinh7a@users.noreply.github.com>
2020-11-20 00:19:54 +0700

    Delete collections.js

--- routes/collections.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-
-/* GET home page. */
-router.get('/', productController.index);
-
-module.exports = router;
